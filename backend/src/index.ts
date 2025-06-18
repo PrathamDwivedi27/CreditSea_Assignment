2b2582425218f4ab3f1894db3a3038ed32ca945a
Pratham-fursatAI <prathamd549@gmail.com>
2025-06-18 12:32:34 +0530

    [Feat]: Server is up and running

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,20 +2,36 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+import { PORT } from './config/server-config';
+import logger from './utils/logger';
+
 
 const app=express();
+app.use(cors());
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-import { PORT } from './config/server-config';
-
-
 const setup_and_start_server=()=>{
     app.listen(PORT,()=>{
-        
+        logger.info(`Server is running on port ${PORT}`);
     })
     
 }
+setup_and_start_server();
+
+const handleServerShutdown=async ()=>{
+    try {
+        logger.info("Shutting down server...");
+        logger.info("Server shutdown complete.");
+        process.exit(0);
+    } catch (error) {
+        logger.error("Error during server shutdown:", error);
+        process.exit(1);
+    }
+}
+
+process.on('SIGINT', handleServerShutdown);
+process.on('SIGTERM', handleServerShutdown);
