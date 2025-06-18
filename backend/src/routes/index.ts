474a3cffcfe626aa8190cc4625935f260ee5128c
Pratham-fursatAI <prathamd549@gmail.com>
2025-06-18 20:25:34 +0530

    [fix]: fix the routers

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
+import authRoutes from "./v1/authRoutes";
+import applicationRoutes from "./v1/applicationRoutes";
+import adminRoutes from "./v1/adminRoutes";
+import verifierRoutes from "./v1/verifierRoutes";
+import loanRoutes from "./v1/loanRoutes";
+
 
 const router = Router();
 
-
+router.use("/v1", authRoutes);
+router.use("/v1", applicationRoutes);
+router.use("/v1", adminRoutes);
+router.use("/v1", verifierRoutes);
+router.use("/v1", loanRoutes);
 
 
 export default router;
